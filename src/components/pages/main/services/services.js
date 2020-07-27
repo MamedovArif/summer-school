907a41d82ec8576c21386606f3a0d7f0a127c87d
MamaedovArif <arifmho@yandex.ru>
2020-07-27 15:21:08 +0300

    this do not work

--- src/components/pages/main/services/services.js
+++ src/components/pages/main/services/services.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
 import ErrorButton from '../../../error-button';
 
@@ -38,64 +38,65 @@ const Credit = () => {
   )
 }
 
-export default class Services extends Component {
-
-  state = {
-    selectedItem: 'delivery',
-  }
-
-  handleChange = (evt) => {
-    const id = evt.target.id;
-    this.setState({
-      selectedItem: id
-    })
-  }
-
-  render() {
-    const {selectedItem} = this.state;
-    const guarantee = selectedItem === 'guarantee' ? <Guarantee /> : null;
-    const transfer = selectedItem === 'delivery' ? <Transfer /> : null;
-    const credit = selectedItem === 'credit' ? <Credit /> : null;
-
-    return (
-      <div className="out-servis">
-        <section className="servis">
-          <div className="servis-container">
-            <h2>Сервисы</h2>
-            <p>Хороший интернет-магазин отличается от плохого не только ценами!
-             Мы стараемся изо всех сил, чтобы сделать ваши покупки приятными.
-            </p>
-          </div>
-          <div className="servis-slide">
-            <ul className="servis-menu">
-              <li>
-                <input onChange={this.handleChange}
-                className="visually-hidden input-radio"
-                type="radio" name="service" id="delivery" defaultChecked/>
-                <label htmlFor="delivery">Доставка</label>
-              </li>
-              <li>
-                <input onChange={this.handleChange}
-                className="visually-hidden input-radio"
-                type="radio" name="service" id="guarantee" />
-                <label htmlFor="guarantee">Гарантия</label>
-              </li>
-              <li>
-                <input onChange={this.handleChange}
-                className="visually-hidden input-radio"
-                type="radio" name="service" id="credit" />
-                <label htmlFor="credit">Кредит</label>
-              </li>
-            </ul>
-            <ul className="servis-content">
-              {guarantee}
-              {transfer}
-              {credit}
-            </ul>
-          </div>
-        </section>
-      </div>
-    )
-  }
+const handleChange = (evt) => {
+  return evt.target.id;
 }
-//
+
+const Services = () => {
+  const [selectedItem, setSelectedItem] = useState('delivery')
+
+  const guarantee = selectedItem === 'guarantee' ? <Guarantee /> : null;
+  const transfer = selectedItem === 'delivery' ? <Transfer /> : null;
+  const credit = selectedItem === 'credit' ? <Credit /> : null;
+
+  return (
+    <div className="out-servis">
+      <section className="servis">
+        <div className="servis-container">
+          <h2>Сервисы</h2>
+          <p>Хороший интернет-магазин отличается от плохого не только ценами!
+           Мы стараемся изо всех сил, чтобы сделать ваши покупки приятными.
+          </p>
+        </div>
+        <div className="servis-slide">
+          <ul className="servis-menu">
+            <li>
+              <input onChange={(evt) => {
+                const id = handleChange(evt);
+                setSelectedItem(id)
+              }}
+              className="visually-hidden input-radio"
+              type="radio" name="service" id="delivery" defaultChecked/>
+              <label htmlFor="delivery">Доставка</label>
+            </li>
+            <li>
+              <input onChange={(evt) => {
+                const id = handleChange(evt);
+                setSelectedItem(id)
+              }}
+              className="visually-hidden input-radio"
+              type="radio" name="service" id="guarantee" />
+              <label htmlFor="guarantee">Гарантия</label>
+            </li>
+            <li>
+              <input onChange={(evt) => {
+                const id = handleChange(evt);
+                setSelectedItem(id)
+              }}
+              className="visually-hidden input-radio"
+              type="radio" name="service" id="credit" />
+              <label htmlFor="credit">Кредит</label>
+            </li>
+          </ul>
+          <ul className="servis-content">
+            {guarantee}
+            {transfer}
+            {credit}
+          </ul>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Services;
